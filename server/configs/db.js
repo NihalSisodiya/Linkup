2a50186d078d9a2d7d73235d9e61f723f391c1c0
NihalSisodiya <139050214+NihalSisodiya@users.noreply.github.com>
2026-04-21 19:24:26 +0530

    Add files via upload

---
+++ server/configs/db.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+    try {
+        mongoose.connection.on('connected', () => console.log('Database Connected Successfully'))
+        await mongoose.connect(`${process.env.MONGO_URI}/linka`)
+    } catch (error) {
+        console.log(error.message)
+    }
+}
+
+export default connectDB;
---
+++ server/controllers/storyController.js
@@ -0,0 +1,71 @@
+import fs from 'fs';
+import imagekit from '../configs/imagekit.js';
+import User from '../models/User.js';
+import Story from '../models/Story.js';
+import { inngest } from '../inngest/index.js';
+
+
+// Add User Story
+export const addUserStory = async (req, res) => {
+    try {
+
+        const {userId} = req.auth();
+        const {content, media_type, background_color} = req.body();
+        const media = req.file;
+        let media_url = '';
+
+        // upload media to imagekit
+        if(media_type === 'image' || media_type === 'video'){
+            const fileBuffer = fs.readFileSync(media.file);
+            const response = await imagekit.upload({
+                file: fileBuffer,
+                fileName: media.originalname,
+            })
+            media_url = response.url
+        }
+
+        // create Story
+        const Story = await Story.create({
+            user: userId,
+            content,
+            media_url,
+            media_type,
+            background_color
+        })
+
+        // schedule story deletion after 24 hours
+        await inngest.send({
+            name: 'app/story.delete',
+            data: {storyId: story._id} 
+        })
+
+        res.json({success: true, message: "Story created successfully"})
+        
+    } catch (err) {
+        console.log(err);
+        res.json({success: false, message: err.message})
+    }
+}
+
+
+// Get User Stories
+export const getStories = async (req, res) => {
+    try {
+
+        const {userId} = req.auth();
+        const user = await User.findById(userId);
+
+        // User connections and following
+        const {userIds} = {userId, ...user.connections, ...user.following};
+
+        const stories = await Story.find({
+            user: {$in: userIds},
+        }).populate('user').sort({createdAt: -1});
+        
+        res.json({success: true, stories});
+
+    } catch (err) {
+        console.log(err);
+        res.json({success: false, message: err.message})
+    }
+}
